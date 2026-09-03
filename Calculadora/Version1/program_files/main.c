2f560fc9585628a2ab9d95f01d0a88ee8d771e0d
MateusLBarreto <269456656+MateusLBarreto@users.noreply.github.com>
2026-09-03 09:57:06 -0300

    main.c atualizado. / Updated main.c

--- Calculadora/Version1/program_files/main.c
+++ Calculadora/Version1/program_files/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int len = 10;
 int poten_10(int exp){
     int temp  = exp;
     int resul = 1;
@@ -23,43 +21,47 @@ int array_into_int(int array[], int len){
     // done - do not edit
 }
 
-int read_line(FILE *ptr){
+char* read_line(FILE *ptr){
     char letra;
-    int  position = 0;
-    int char_invalido = 0;
+    int  i             = 0;
+    int  position      = 0;
+    int  char_invalido = 0;
 
-    ptr = fopen("number.txt", "r+");
-    while(fscanf(ptr, "%c", &letra) != EOF && fscanf(ptr, "%c", &letra) != '\n' || char_invalido != 0){
-        if (fscanf(ptr, "%c", &letra) == '\n'){
-            char_invalido++;
-        }
-        else{
-            fseek(ptr, position, SEEK_SET);
-            fscanf(ptr, "%c", &letra);
-            printf("%c", letra);
-            position++;
-        }
+    ptr = fopen("number.txt", "r");
+    while(fscanf(ptr, "%c", &letra) != EOF && fscanf(ptr, "%c", &letra) != '\n'){
+        fseek(ptr, position, SEEK_SET);
+        fscanf(ptr, "%c", &letra);
+        printf("%c", letra);
+        position++;
     }
-    // char line[position];
-    // position = 0;
-    // while(fscanf(ptr, "%c", &letra) != EOF){
-    //     fseek(ptr, position, SEEK_SET);
-    //     fscanf(ptr, "%c", &letra);
-    //     line[position] = letra;
-    //     position++;
-    // }
+    char line[position];
+    position = 0;
+    while(fscanf(ptr, "%c", &letra) != EOF){
+        fseek(ptr, position, SEEK_SET);
+        fscanf(ptr, "%c", &letra);
+        line[position] = letra;
+        position++;
+    }
     // this part of the code will be used to return the line read bt this function
 
-    // current problems :
-    //  - function read entire file, not first line
-
     fclose(ptr);
-    return 0;
+    return line;
 }
 int main(){
     FILE *point;
-    read_line(point);
+
+    int  i         = 0;
+    char arquivo[] = read_line(point);
+
+    for (i = 0; i < sizeof arquivo / sizeof(char); i++){
+        printf("%c\n", arquivo[i]);
+    }
+    
     // running this code will read the entire 'number.txt' file and print it in the terminal
 
     return 0;
 }
+    // current problems :
+    //  - function readline() reads entire file, not first line
+    //  - function readline() returns (null)
+    //  - in main, char arquivo gives 'invalid initializer' as a error code
